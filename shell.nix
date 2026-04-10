57d0fe14e2f6d83ea1c43bb70e3af25ab06e91e1
mmuffins <mail@tiv73.com>
2026-04-10 22:40:05 +0200

    sdk

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 { pkgs ? import <nixpkgs> {} }:
 let
   buildInputs = with pkgs; [
-    dotnetCorePackages.sdk_8_0
+    dotnetCorePackages.sdk_10_0
     omnisharp-roslyn
     nuget-to-json
     # stdenv.cc
@@ -11,7 +11,7 @@ in
   inherit buildInputs;
     name = "dotnet-env";
 
-    DOTNET_ROOT = "${pkgs.dotnetCorePackages.sdk_8_0}";
+    DOTNET_ROOT = "${pkgs.dotnetCorePackages.sdk_10_0}";
 
     shellHook = ''
       export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath buildInputs}
